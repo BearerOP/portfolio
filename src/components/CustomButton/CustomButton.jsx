66bcb5ab1fa71ea541075df24c80d81a2105818f
BearerOP <ankity1892003@gmail.com>
2024-08-24 04:18:42 +0530

    css modified

--- src/components/CustomButton/CustomButton.jsx
+++ src/components/CustomButton/CustomButton.jsx
@@ -3,9 +3,13 @@ import ArrowForwardOutlinedIcon from "@mui/icons-material/ArrowForwardOutlined";
 
 const CustomButton = ({ text, size, link }) => {
   const handleScroll = () => {
+    console.log(
+      "You clicked the button and scrolled to the link: ",
+    );
+    
     const element = document.getElementById(link);
     if (element) {
-      element.scrollIntoView({ behavior: "" });
+      element.scrollIntoView({ behavior: "smooth" });
     }
   };
   return (
